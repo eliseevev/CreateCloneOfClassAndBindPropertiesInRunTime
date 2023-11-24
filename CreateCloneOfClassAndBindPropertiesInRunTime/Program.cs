c9249768b22e8bd99b587fa472d2b37bbd5fac6c
Eliseev Eugene <eliseevmail@ya.ru>
2023-11-25 01:30:10 +0300

    Добавьте файлы проекта.

---
+++ CreateCloneOfClassAndBindPropertiesInRunTime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace CreateCloneOfClassAndBindPropertiesInRunTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var model = new TestModel() { Name = "some_name", Description = "some_desc" };
+
+            var cloner = new Cloner<TestModel>();
+            var clone = cloner.CreateCopy(model);
+
+
+            Console.WriteLine("Model - " + JsonSerializer.Serialize(model));
+            Console.WriteLine("Clone - " + JsonSerializer.Serialize(clone));
+        }
+    }
+
+    internal class TestModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Number { get; set; }
+    }
+}
